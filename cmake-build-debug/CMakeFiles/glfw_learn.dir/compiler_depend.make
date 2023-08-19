cc4f7f1ee2fba232d44a69b6d6f0d8f2bde51ab8
Mike <mlgexgood@gmail.com>
2023-08-20 04:10:27 +0600

    GLEW

---
+++ cmake-build-debug/CMakeFiles/glfw_learn.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for glfw_learn.
+# This may be replaced when dependencies are built.
--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>
 
-#include <GL\glew.h>
-#include <GLFW\glfw3.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
